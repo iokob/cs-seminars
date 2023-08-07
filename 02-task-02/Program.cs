3a6918f9b3a632b258b2aa427725d7aa55714e29
ioko <ikobozev3@gmail.com>
2023-08-07 17:52:06 +0300

    task-02 complete!

--- 02-task-02/Program.cs
+++ 02-task-02/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите 2 числа: ");
+double a = Convert.ToDouble(Console.ReadLine());
+double b = Convert.ToDouble(Console.ReadLine());
+    if (a == b*b) {
+        Console.WriteLine("Число a является квадратом числа b");
+    }
+    else {
+        Console.WriteLine("Число a не является квадратом числа b");
+    }
