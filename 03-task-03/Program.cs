26b892abf529a50bea5048dc810bb1405b2e7232
ioko <ikobozev3@gmail.com>
2023-08-07 21:29:44 +0300

    task-03 - number to day complete

---
+++ 03-task-03/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Число -> День недели. Число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number == 1) {
+    Console.WriteLine("Понедельник");
+} else if (number == 2) {
+    Console.WriteLine("Вторник");
+} else if (number == 3) {
+    Console.WriteLine("Среда");
+} else if (number == 4) {
+    Console.WriteLine("Четверг");
+} else if (number == 5) {
+    Console.WriteLine("Пятница");
+} else if (number == 6) {
+    Console.WriteLine("Суббота");
+} else if (number == 7) {
+    Console.WriteLine("Воскресение");
+} else {
+    Console.WriteLine("Неверный ввод");
+}
