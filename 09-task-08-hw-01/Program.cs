09171ccc9bd2cf603e3875b0e6912f967639ac12
ioko <ikobozev3@gmail.com>
2023-08-10 00:29:14 +0300

    task 08 homework 01 completed

---
+++ 09-task-08-hw-01/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("* * * Введите целое число и нажмите enter * * *");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("");
+for (int i = 0; i <= N; i++) {
+    if (i % 2 == 0) {
+        Console.WriteLine(i + " ");
+    }
+}
+Console.WriteLine("* * * Работа программы завершена * * *");
