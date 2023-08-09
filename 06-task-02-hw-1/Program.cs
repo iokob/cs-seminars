629dff9bebe8040078825c35770e41ec98295b6f
ioko <ikobozev3@gmail.com>
2023-08-09 23:44:55 +0300

    task 02 hw-1 completed

---
+++ 06-task-02-hw-1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("* * * Введите 2 числа, разделяя их enter * * *");
+double a = Convert.ToDouble(Console.ReadLine());
+double b = Convert.ToDouble(Console.ReadLine());
+    if (a > b) {    
+        Console.WriteLine("Число " + a + " большее");
+        Console.WriteLine("Число " + b + " меньшее");
+    }
+    else if (a < b) {
+        Console.WriteLine("Число " + b + " большее");
+        Console.WriteLine("Число " + a + " меньшее");
+    }
+    else if (a == b) {
+        Console.WriteLine("Числа равны");
+    }
+Console.WriteLine("* * * Работа программы завершена * * *");
