ecab4c4aad9ee605991164b426485d47cc17311a
ioko <ikobozev3@gmail.com>
2023-08-10 00:07:11 +0300

    task 06 completed (homework 01)

---
+++ 08-task-06-hw-01/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("* * * Введите целое число и нажмите enter * * *");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N % 2 == 0) {
+    Console.WriteLine("Число " + N + " является чётным");
+} else {
+    Console.WriteLine("Число " + N + " не является чётным");
+}
+
+Console.WriteLine("* * * Работа программы завершена * * *");
