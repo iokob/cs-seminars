9af1a7588807caec23b4d210ed21b7f813569bc2
ioko <ikobozev3@gmail.com>
2023-08-10 00:00:24 +0300

    task 04 homework 01 completed

---
+++ 07-task-04-hw-01/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("* * * Введите 3 числа, разделяя их enter * * *");
+double a = Convert.ToDouble(Console.ReadLine());
+double b = Convert.ToDouble(Console.ReadLine());
+double c = Convert.ToDouble(Console.ReadLine());
+double max = a;
+if (max < b) {
+    max = b;
+}
+else if (max < c) {
+    max = c;
+}
+Console.WriteLine("Максимальное из этих трёх чисел число " + max);
+Console.WriteLine("* * * Работа программы завершена * * *");
