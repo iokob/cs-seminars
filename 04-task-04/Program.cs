772932084fd13b0b50f1d806d92e4d7919a9d8c9
ioko <ikobozev3@gmail.com>
2023-08-10 00:32:41 +0300

    some folders renamed and README updated

--- 04-task-04/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿Console.WriteLine("Введите натуральное число N");
-int number = Convert.ToInt32(Console.ReadLine());
-for (int i = -number; i <= number; i++)   {
-    Console.Write(i + " ");
-}
---
+++ 04-task-05/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите натуральное число N");
+int number = Convert.ToInt32(Console.ReadLine());
+for (int i = -number; i <= number; i++)   {
+    Console.Write(i + " ");
+}
