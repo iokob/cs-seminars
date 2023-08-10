a71796163944b46a163296b495e117053ce09601
ioko <ikobozev3@gmail.com>
2023-08-10 04:13:54 +0300

    task 12 + (se 02)

---
+++ 10-task-12-se-02/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("* * * Введите 2 целых числа, разделяя их enter * * *");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+void func(int a, int b) {
+    if (b % a == 0) {
+        Console.WriteLine("Второе число кратно первому");
+    } 
+    else {
+        Console.WriteLine("Остаток от деления = " + b % a);
+    }
+}
+func(a, b);
+Console.WriteLine("* * * Работа программы завершена * * *");
