42bf45d3b71d3baa58db5cd1cddffc263ef129c7
ioko <ikobozev3@gmail.com>
2023-08-10 04:18:21 +0300

    output edited

--- 10-task-12-se-02/Program.cs
+++ 10-task-12-se-02/Program.cs
@@ -3,7 +3,7 @@ int a = Convert.ToInt32(Console.ReadLine());
 int b = Convert.ToInt32(Console.ReadLine());
 void func(int a, int b) {
     if (b % a == 0) {
-        Console.WriteLine("Второе число кратно первому");
+        Console.WriteLine($"Число {b} кратно числу {a}");
     } 
     else {
         Console.WriteLine("Остаток от деления = " + b % a);
