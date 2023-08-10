aaa8f84fb496e411ee2fefc5179298a375c94545
ioko <ikobozev3@gmail.com>
2023-08-10 06:25:56 +0300

    task 13 +

---
+++ 16-task-13-hw-02/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("* * * Введите число и нажмите enter * * *");
+string number = Console.ReadLine();
+string func(string a) {
+    if (number.Length >= 3) {
+        return "Третья цифра числа: " + number[2];
+    }
+    else return "Третьей цифры нет";
+}
+Console.WriteLine(func(number));
+Console.WriteLine("* * * Работа программы завершена * * *");
