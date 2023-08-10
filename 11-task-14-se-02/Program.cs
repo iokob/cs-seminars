c73230f50bb45aeabd4c3c8d9f535001a173b6e0
ioko <ikobozev3@gmail.com>
2023-08-10 04:27:07 +0300

    task 14 +

---
+++ 11-task-14-se-02/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("* * * Введите Число и нажмите enter * * *");
+int number = Convert.ToInt32(Console.ReadLine());
+bool func(int a) {
+    if (a % 7 == 0 && a % 23 == 0) return true;
+    else return false;
+}
+Console.WriteLine(func(number));
+Console.WriteLine("* * * Работа программы завершена * * *");
