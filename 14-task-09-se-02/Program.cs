860ec0966993621c780092bc01cec62833f06457
ioko <ikobozev3@gmail.com>
2023-08-10 05:14:04 +0300

    task 09 +

---
+++ 14-task-09-se-02/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("* * * Старт работы программы * * *");
+int rnd = new Random().Next(10, 100);
+int func(int a) {
+    int s1 = a / 10;
+    int s2 = a % 10;
+    int max = s1;
+    if (s2 > max) max = s2;
+    return max;
+}
+Console.WriteLine(rnd + "->" + func(rnd));
+Console.WriteLine("* * * Работа программы завершена * * *");
