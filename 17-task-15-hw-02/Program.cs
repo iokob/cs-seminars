21e4c57442225359fca55ae37e94515591783bd6
ioko <ikobozev3@gmail.com>
2023-08-10 06:31:57 +0300

    task 15 +

---
+++ 17-task-15-hw-02/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("* * * Введите число от 1 до 7 и нажмите enter * * *");
+byte number = Convert.ToByte(Console.ReadLine());
+bool func(byte a) {
+    if (a >= 6) {
+        return true;
+    }
+    else return false;
+}
+Console.WriteLine(func(number));
+Console.WriteLine("* * * Работа программы завершена * * *");
