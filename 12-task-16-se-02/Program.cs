108e35c720d603ea591c73cc2a6d6bc5cff2a2fd
ioko <ikobozev3@gmail.com>
2023-08-10 04:38:16 +0300

    task 16 +

---
+++ 12-task-16-se-02/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("* * * Введите 2 целых числа, разделяя их enter * * *");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+
+bool func(int a, int b) {
+    if (a == b*b || b == a*a) return true;
+    else return false;
+}
+Console.WriteLine(func(a,b));
+Console.WriteLine("* * * Работа программы завершена * * *");
