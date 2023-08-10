86b4d9f17c387788c364e959189d28d7a4d9bec3
ioko <ikobozev3@gmail.com>
2023-08-10 04:58:08 +0300

    13-task-x +

---
+++ 13-task-x-se-02/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("* * * Старт работы программы * * *");
+int rnd3 = new Random().Next(100, 1000);
+string func(int a) {
+    string s1 = Convert.ToString(a / 100);
+    string s2 = Convert.ToString(a % 10);
+    return s1 + s2;
+}
+Console.WriteLine(rnd3 + "->" + func(rnd3));
+Console.WriteLine("* * * Работа программы завершена * * *");
