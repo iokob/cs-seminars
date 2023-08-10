e069e43ea82726607a0d4f89271167093bc53c5d
ioko <ikobozev3@gmail.com>
2023-08-10 06:08:38 +0300

    task 10 +

---
+++ 15-task-10-hw-02/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("* * * Введите трёхзначное число и нажмите enter * * *");
+int number = Convert.ToInt32(Console.ReadLine());
+int func(int a) {
+    return (a / 10) % 10;
+}
+Console.WriteLine("Вторая цифра числа " + func(number));
+Console.WriteLine("* * * Работа программы завершена * * *");
