8e71006b7ef54fe117e657520d675f84ca118e5e
ioko <ikobozev3@gmail.com>
2023-08-25 23:45:20 +0300

    task 17 completed

---
+++ 18-se-03-task-17/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("* * * Введите координаты x и y, разделяя их enter * * *");
+double x = Convert.ToDouble(Console.ReadLine());
+double y = Convert.ToDouble(Console.ReadLine());
+if (x == 0 || y == 0) {
+    Console.WriteLine("Одна или две координаты равны нулю");
+} else {
+    if (x > 0) {
+        if (y > 0) {
+            Console.WriteLine("Точка находится в I четверти");
+        } else 
+        if (y < 0) {
+            Console.WriteLine("Точка находится в IV четверти");
+        }
+    } else
+    if (x < 0) {
+        if (y > 0) {
+            Console.WriteLine("Точка находится во II четверти");
+    } else 
+        if (y < 0) {
+            Console.WriteLine("Точка находится в III четверти");
+        }
+        }
+}
+
+Console.WriteLine("* * * Работа программы завершена * * *");
