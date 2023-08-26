5a2c18ff43d5750ee7fc074f76ccc895c9760317
ioko <ikobozev3@gmail.com>
2023-08-26 03:11:13 +0300

    task x completed (19 part)

---
+++ 19-se-03-task-x/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("* * * Введите номер четверти декартовой системы координат (1-4) * * *");
+int quart = Convert.ToInt32(Console.ReadLine());
+if (quart == 1) {
+    Console.WriteLine("x > 0, y > 0");
+} else
+if (quart == 2) {
+    Console.WriteLine("x < 0, y > 0");
+} else
+if (quart == 3) {
+    Console.WriteLine("x < 0, y < 0");
+} else
+if (quart == 4) {
+    Console.WriteLine("x > 0, y < 0");
+}
+
+Console.WriteLine("* * * Работа программы завершена * * *");
