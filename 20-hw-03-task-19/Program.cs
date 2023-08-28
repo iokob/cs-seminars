13000a8980b1267e910285bb776f936e04dc0196
ioko <ikobozev3@gmail.com>
2023-08-29 00:28:05 +0300

    20 21 22

---
+++ 20-hw-03-task-19/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("* * * Введите пятизначное число * * *");
+int number = Convert.ToInt32(Console.ReadLine());
+bool noflag = false;
+
+for (int i = 0; i < 5/2;) {
+    if (number / (Math.Pow(10, 5-1-i)) != number % (Math.Pow(10, i+1))) {
+        Console.WriteLine($"Число {number} не является палиндромом");
+        noflag = true;
+        break;
+    } else {
+        i++;
+    }
+    if (noflag) {break;}
+    Console.WriteLine($"Число {number} является палиндромом");
+}
+
+Console.WriteLine("* * * Работа программы завершена * * *");
