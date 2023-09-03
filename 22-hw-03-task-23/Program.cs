20ead7050fdef050260dffb2bd85040601794704
ioko <ikobozev3@gmail.com>
2023-09-03 19:29:37 +0300

    task 23 completed

--- 22-hw-03-task-23/Program.cs
+++ 22-hw-03-task-23/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("* * * Введите число N * * *");
+int number = Convert.ToInt32(Console.ReadLine());
+int[] cubes = new int[number];
+
+for (int i = 0; i <= number; i++) {
+    cubes[i] = Convert.ToInt32(Math.Pow(i+1, 3));
+Console.WriteLine($"* * * i^3 = {cubes[i]} * * *");
+}
+
+
+
+
+Console.WriteLine("* * * Работа программы завершена * * *");
