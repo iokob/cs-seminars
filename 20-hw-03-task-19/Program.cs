ae2ee73a7d913db816f8c559fad19c70dbf1ae53
ioko <ikobozev3@gmail.com>
2023-09-05 07:37:53 +0300

    task 19 completed

--- 20-hw-03-task-19/Program.cs
+++ 20-hw-03-task-19/Program.cs
@@ -1,17 +1,28 @@
 ﻿Console.WriteLine("* * * Введите пятизначное число * * *");
 int number = Convert.ToInt32(Console.ReadLine());
-bool noflag = false;
-
-for (int i = 0; i < 5/2;) {
-    if (number / (Math.Pow(10, 5-1-i)) != number % (Math.Pow(10, i+1))) {
-        Console.WriteLine($"Число {number} не является палиндромом");
-        noflag = true;
-        break;
-    } else {
-        i++;
-    }
-    if (noflag) {break;}
-    Console.WriteLine($"Число {number} является палиндромом");
+int countDigits (int number) {
+    return Convert.ToInt32(Math.Log10(number)) + 1;
 }
+int nD = countDigits(number);
+int rigthDig = 0;
+int leftDig = 0;
+bool funcPass = false;
+for (int i = 1; i <= nD/2; i++) {
+    funcPass = false;
+    rigthDig = number / Convert.ToInt32(Math.Pow(10, i-1)) % 10;
+    leftDig = number / Convert.ToInt32(Math.Pow(10, nD-i)) % 10;
+
+    Console.WriteLine($"Math.Pow(10, nD-1-i): {Convert.ToInt32(Math.Pow(10, nD-i))}"); 
+   Console.WriteLine($"rl digs: {rigthDig} .. {leftDig}"); 
+    if (rigthDig == leftDig) {
+        funcPass = true;
+    }
+    else {
+        break;
+        }
+    }
+
+
+Console.WriteLine($"Число {number} - палиндром: {funcPass}");
 
 Console.WriteLine("* * * Работа программы завершена * * *");
