ed0430491c5e8a50c1acc77afbe9423205d6f54f
ioko <ikobozev3@gmail.com>
2023-09-10 23:21:59 +0300

    task 26 compl

--- 24-se-04-task-26/Program.cs
+++ 24-se-04-task-26/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("* * * Введите число N * * *");
+int N = Convert.ToInt32(Console.ReadLine());
+int calcDigits (int n){
+    return Convert.ToInt32(Math.Log10(n)) + 1;
+}
+
+Console.WriteLine($"количество цифр в числе равно {calcDigits(N)}");
+
+Console.WriteLine("* * * Работа программы завершена * * *");
