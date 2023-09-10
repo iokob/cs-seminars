aa2d809bea4b5b6d9f1cbe71596fb483873810f8
ioko <ikobozev3@gmail.com>
2023-09-10 23:45:46 +0300

    task 28 +

---
+++ 25-se-04-task-28/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("* * * Введите число N * * *");
+int N = Convert.ToInt32(Console.ReadLine());
+int calc (int n){
+    int res = 1;
+    for (int i = 1; i <= n; i++){
+        res = res * i;
+    }
+    return res;
+}
+
+Console.WriteLine($"количество произведение чисел от 1 до {N} равно {calc(N)}");
+
+Console.WriteLine("* * * Работа программы завершена * * *");
