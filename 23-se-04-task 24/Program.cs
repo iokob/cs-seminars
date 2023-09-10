70a9f80a1c4a31e0e6c817cecae000ccd926749f
ioko <ikobozev3@gmail.com>
2023-09-10 21:13:55 +0300

    23-task-24 compl

---
+++ 23-se-04-task 24/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("* * * Введите число А * * *");
+int a = Convert.ToInt32(Console.ReadLine());
+int sum1toa (int a) {
+    int sum = 0;
+    for (int i = 1; i <= a; i++) {
+        sum = sum + i;
+    }
+    return sum;
+}
+Console.WriteLine($"сумма чисел от 1 до A = {sum1toa(a)}");
+
+Console.WriteLine("* * * Работа программы завершена * * *");
