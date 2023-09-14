fbb5a9e0896a0e9315e86ebe25682fe8e4581cda
ioko <ikobozev3@gmail.com>
2023-09-14 22:57:38 +0300

    task 30

---
+++ 26-se-04-task-30/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("* * * Начало программы * * *");
+
+int[] array = new int[8];
+int[] GetRandomArray(int[] array) {
+    Random rdn = new Random();
+    for (int i = 0; i < array.Length; i++){
+        array[i] = rdn.Next(2);
+    }
+    return array;
+}
+
+void PrintArray(int[] array) {
+    for (int i = 0; i < array.Length; i++){
+          Console.WriteLine(Convert.ToInt32(array[i]));
+    }
+}
+
+array = GetRandomArray(array);
+PrintArray(array);
+
+
+Console.WriteLine("* * * Работа программы завершена * * *");
